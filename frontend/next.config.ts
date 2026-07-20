fad4b0a17a2fd2198c458c7ca473bbec4664f9b8
Ilya <Bezzubik12345@i.ua>
2026-07-20 18:14:54 +0300

    fix: make redirect to /dashboard on / path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1", "test-frontend", "frontend"],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
